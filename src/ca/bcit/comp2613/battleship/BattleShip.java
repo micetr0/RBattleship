878fcf52af7e08847e0230d7507efacf87596194
Androo <aruan.school@gmail.com>
2014-07-24 01:44:56 -0700

    editted battleship to show all frames created so far

--- src/ca/bcit/comp2613/battleship/BattleShip.java
+++ src/ca/bcit/comp2613/battleship/BattleShip.java
@@ -13,16 +13,28 @@ public class BattleShip {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        //Menu menu = new Menu();
+        Board board = new Board();
+        JPanel ctrlPanel = new JPanel();
+        ctrlPanel.setSize(850, 150);
+        
+        JFrame g = new JFrame("test");
+        g.setSize(850, 1000);
+        g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        g.setVisible(true);
+        g.add(board);
+        g.add(ctrlPanel);
+        
         SetupBoard setup = new SetupBoard();
-      Board board = new Board();
-      JPanel ctrlPanel = new JPanel();
-      ctrlPanel.setSize(850, 150);
+
       JFrame f = new JFrame("test");
       f.setSize(850, 500);
       f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       f.setVisible(true);
       f.add(setup);
+      
+
+      
+      Menu menu = new Menu();
       }
     
 //        Board board = new Board();
